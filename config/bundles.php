f165ea0b17f2bac83371c5cc7b2b52d8331476c3
igor <kryvoruchko.dev@gmail.com>
2020-12-26 15:13:50 +0200

    log

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    BoShurik\TelegramBotBundle\BoShurikTelegramBotBundle::class => ['all' => true],
 ];
