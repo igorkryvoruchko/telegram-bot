f71bae2711fb4f189f03bee16a5fcf13acb22c56
igor <kryvoruchko.dev@gmail.com>
2020-12-26 14:26:42 +0200

    bot route

--- src/Controller/BotController.php
+++ src/Controller/BotController.php
@@ -9,12 +9,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class BotController extends AbstractController
 {
     /**
-     * @Route("/", name="bot")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/BotController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/bot", name="bot")
+     */
+    public function bot(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new bot controller!',
             'path' => 'src/Controller/BotController.php',
         ]);
     }
