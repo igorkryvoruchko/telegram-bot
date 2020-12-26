e713073628db8200505553f5d2e1e755bd6e246d
igor <kryvoruchko.dev@gmail.com>
2020-12-26 16:13:05 +0200

    Borsaco

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     BoShurik\TelegramBotBundle\BoShurikTelegramBotBundle::class => ['all' => true],
+    Borsaco\TelegramBotApiBundle\TelegramBotApiBundle::class => ['all' => true],
 ];
--- src/Controller/BotController.php
+++ src/Controller/BotController.php
@@ -7,19 +7,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Log\Logger;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Telegram\Bot\Api;
+use Borsaco\TelegramBotApiBundle\Service\Bot;
+
 
 class BotController extends AbstractController
 {
     /**
-     * @Route("/", name="bot")
+     * @Route("/", methods={"POST"}, name="bot")
+     * @param Bot $bot
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException
      */
-    public function telegram(Request $request, Logger $logger): JsonResponse {
-        $result = json_decode($request->getContent(), true);
-        $logger->info($request->getContent());
-        return new JsonResponse([]);
+    public function telegram(Bot $bot, Request $request): JsonResponse
+    {
+        $response = json_decode((string)$request->getContent());
+        $firstBot = $bot->getBot('first');
+        if(!empty($response) && !empty($response->message)){
+            $userId = $response->message->from->id;
+            $firstBot->sendMessage(['chat_id' => $userId, 'text' => 'hi']);
+        }
+        return new Response('ok');
     }
 
 
