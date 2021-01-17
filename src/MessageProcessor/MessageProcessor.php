82ad52823d910eadcfc7cb8b79a4d7c587bc9ce0
igor <kryvoruchko.dev@gmail.com>
2021-01-17 18:11:04 +0200

    buttons

--- src/MessageProcessor/MessageProcessor.php
+++ src/MessageProcessor/MessageProcessor.php
@@ -31,9 +31,16 @@ class MessageProcessor
     {
         if(!empty($response) && !empty($response->message)){
             $keyboard = new Keyboard();
-            $replay_keyboard = $keyboard->make(['resize_keyboard' => true])->row(
-                $keyboard->Button(['text' => 'button1'])
-            );
+            $replay_keyboard = $keyboard->make(['resize_keyboard' => true])
+                ->row(
+                    $keyboard->Button(['text' => '/кто красавчик?']),
+                    $keyboard->Button(['text' => '/пакет'])
+                )
+                ->row(
+                    $keyboard->Button(['text' => '/танцевать']),
+                    $keyboard->Button(['text' => '/курс валют'])
+                )
+            ;
             $userId = $response->message->from->id;
             $messageText = mb_strtolower($response->message->text);
             switch ($messageText){
@@ -41,28 +48,31 @@ class MessageProcessor
                     $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'Привет', 'reply_markup' => $replay_keyboard]);
                     break;
                 case "/help":
-                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'Чем помочь?']);
+                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'Чем помочь?', 'reply_markup' => $replay_keyboard]);
                     break;
                 case "и че":
                 case "и чё":
-                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'Через плечо']);
+                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'Через плечо', 'reply_markup' => $replay_keyboard]);
                     break;
                 case "спасибо":
-                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'от души братуха!!! от души']);
+                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'от души братуха!!! от души', 'reply_markup' => $replay_keyboard]);
                     break;
-                case "танцевать":
-                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'https://www.youtube.com/watch?v=w9okGAKOyYk']);
+                case "/танцевать":
+                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'https://www.youtube.com/watch?v=w9okGAKOyYk', 'reply_markup' => $replay_keyboard]);
                     break;
-                case "курс валют":
+                case "/курс валют":
                     foreach ($this->getRates() as $rate){
                         $this->bot->sendMessage(['chat_id' => $userId, 'text' => $rate['ccy'].' покупка: '.$rate['buy']. $rate['base_ccy'] . ' продажа: '.$rate['sale']. $rate['base_ccy'] ]);
                     }
                     break;
-                case "пакет":
-                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'https://github.com/borsaco/TelegramBotApiBundle']);
+                case "/пакет":
+                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'https://github.com/borsaco/TelegramBotApiBundle', 'reply_markup' => $replay_keyboard]);
+                    break;
+                case "/кто красавчик?":
+                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'Конечно Игорь Криворучко!', 'reply_markup' => $replay_keyboard]);
                     break;
                 default:
-                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'Я не понимаю о чем вы!']);
+                    $this->bot->sendMessage(['chat_id' => $userId, 'text' => 'Я не понимаю о чем вы!', 'reply_markup' => $replay_keyboard]);
                     break;
             }
 
